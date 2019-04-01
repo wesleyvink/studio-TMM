023046ea1fcfa2ad057e5f14d5a339a9558c0a41
wesley <wesley.vink2001@gmail.com>
2019-04-01 11:33:23 +0200

    werkende single page navigation

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://github.com/kswedberg/jquery-smooth-scroll/blob/master/jquery.smooth-scroll.min.js"></script>
+<script>
+$('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+    
+    return false;
+});
+</script>
+<div id="nav">
+     <ul>
+         <li><a href="#home">home</a></li>
+         <li><a href="#opdrachten">opdrachten</a></li>
+         <li><a href="#agenda">agenda</a></li>
+         <li><a href="#archief">archief</a></li>
+     </ul>
+</div>
+<div id="page1">
+  <a id="home" class="smooth"></a>
+  pagina 1
+</div>
+
+<div id="page2">
+  <a id="opdrachten" class="smooth"></a>
+  pagina 2
+</div>
+
+<div id="page3">
+  <a id="agenda" class="smooth"></a>
+  pagina 3
+  </div>
+<div id="page4">
+  <a id="archief" class="smooth"></a>
+  pagina 4
+  </div>
+</body>
+</html>
