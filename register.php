24151388a7f2bbb600b6c826cdf1b48664503a1b
wesley <wesley.vink2001@gmail.com>
2019-05-13 09:54:09 +0200

    commit

--- register.php
+++ register.php
@@ -1,16 +1,22 @@
 <?php require "dbs/dbconnect.php"; ?>
 <form action="register.php" method="post">
+    Voornaam
     <input type="text" name="vnaam"><br/>
+    Achternaam
     <input type="text" name="anaam"><br/>
+    Geboortedatum
     <input type="date" name="datum"><br/>
+    Email
     <input type="text" name="email"><br/>
+    Wachtwoord
     <input type="password" name="pass"><br/>
+    Wachtwoord opnieuw invoeren
     <input type="password" name="passconfirm"><br/>
     <button type="submit">registreer</button>
 </form>
 
 <h2>
-    heeft u al een account log <a href="register.php">hier</a> in
+    heeft u al een account log <a href="index.php">hier</a> in
 </h2>
 <?php
 if (isset($_POST["vnaam"])) {
@@ -21,16 +27,24 @@ if (isset($_POST["vnaam"])) {
     $pass = $_POST["pass"];
     $passconfirm = $_POST["passconfirm"];
 
-    echo $birthdate;
+    $sql = "select * from `users` where `Email` like ".$email;
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        $row["Email"];
 
-    $sql ="INSERT INTO `users`(`VoorNaam`, `AchterNaam`, `Geboortedatum`, `Email`, `Wachtwoord`)
+    }
+    die();
+    if (0 == $result->num_rows) {
+        $sql = "INSERT INTO `users`(`VoorNaam`, `AchterNaam`, `Geboortedatum`, `Email`, `Wachtwoord`)
     VALUES ('$first','$last','$birthdate','$email','$pass')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        echo "<script type='text/javascript'>location.href = 'index.html';</script>";
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            echo "<script type='text/javascript'>location.href = 'index.php';</script>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "email is al in gebruik";
     }
 }
 ?>
