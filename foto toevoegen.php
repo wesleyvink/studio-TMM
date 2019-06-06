55f80eb625305824a798049f364b767347b1b934
wesley <wesley.vink2001@gmail.com>
2019-06-06 13:52:10 +0200

    mainpage content

---
+++ foto toevoegen.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+require "dbs/dbconnect.php";
+if (isset($_POST['opID2'])){
+    $opID = $_POST['opID2'];
+    $_SESSION['opID2'] = $opID;
+}
+else{
+   // $opID = $_SESSION['opID2'];
+}
+$opID = 1;
+?>
+<html>
+<form action="foto%20toevoegen.php">
+<input type="file" name="opdrachtafb">
+<input type="submit" name="submit">
+</form>
+</html>
+
+<?php
+if(isset($_POST['submit'])) {
+// location.href = 'mainpage.php';
+    $fileExistsFlag = 0;
+    $fileName = $_FILES['opdrachtafb']['name'];
+    $query = "SELECT foto FROM foto WHERE foto='$fileName'";
+    $result = $conn->query($query) or die("Error : " . mysqli_error($conn));
+    while ($row = mysqli_fetch_array($result)) {
+        if ($row['filename'] == $fileName) {
+            $fileExistsFlag = 1;
+        }
+    }
+    if ($fileExistsFlag == 0) {
+        $target = "opafb/";
+        $fileTarget = $target . $fileName;
+        $tempFileName = $_FILES["opdrachtafb"]["tmp_name"];
+        $result = move_uploaded_file($tempFileName, $fileTarget);
+
+        /*
+        *	If file was successfully uploaded in the destination folder
+        */
+        if ($result) {
+            echo "Your file <html><b><i>" . $fileName . "</i></b></html> has been successfully uploaded";
+            $query = "INSERT INTO `foto`(`OpdrachtID`, `foto`, `fotoloc`) VALUES (`$opID`,`$fileName`,`$fileTarget`))";
+            $conn->query($query) or die("Error : " . mysqli_error($conn));
+        } else {
+            echo "Sorry !!! There was an error in uploading your file";
+        }
+        mysqli_close($conn);
+    } else {
+        echo "File <html><b><i>" . $fileName . "</i></b></html> already exists in your folder. Please rename the file and try again.";
+        mysqli_close($conn);
+    }
+}
--- mainpage.php
+++ mainpage.php
@@ -50,76 +50,21 @@ $('.smooth').on('click', function() {
 </div>
 <div id="page1">
   <a id="howme" class="smooth"></a>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-    Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-    Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-    Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-    Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-    Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-    Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
-    Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-    Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
-    Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo.
-    Curabitur a felis in nunc fringilla tristique. Morbi mattis ullamcorper velit. Phasellus gravida semper nisi. Nullam vel sem.
+    <?php include "opdrachten.php";?>
 </div>
 <div id="page2">
   <a id="opdrachten" class="smooth"></a>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-    Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-    Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-    Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-    Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-    Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-    Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
-    Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-    Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
-    Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo.
-    Curabitur a felis in nunc fringilla tristique. Morbi mattis ullamcorper velit. Phasellus gravida semper nisi. Nullam vel sem.
+    <?php include "projecten.php";?>
+    lorem ipsum
 </div>
 <div id="page3">
   <a id="agenda" class="smooth"></a>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-    Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-    Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-    Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-    Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-    Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-    Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
-    Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-    Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
-    Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo.
-    Curabitur a felis in nunc fringilla tristique. Morbi mattis ullamcorper velit. Phasellus gravida semper nisi. Nullam vel sem.
+    <?php include "archief.php"?>
 
   </div>
 <div id="page4">
   <a id="archief" class="smooth"></a>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-    Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-    Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
-    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-    Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-    Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-    Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-    Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-    Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-    Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
-    Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
-    Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
-    Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo.
-    Curabitur a felis in nunc fringilla tristique. Morbi mattis ullamcorper velit. Phasellus gravida semper nisi. Nullam vel sem.
+    <?php include "register.php"?>
   </div>
 </body>
 </html>
