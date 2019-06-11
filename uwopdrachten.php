42060a44f9450f2c41e5caafa8a032fb8516d09c
wesley <wesley.vink2001@gmail.com>
2019-06-11 23:18:22 +0200

    i think i got all of the functions now
    except for 1 fatal bug

--- uwopdrachten.php
+++ uwopdrachten.php
@@ -1,21 +1,25 @@
 <?php
+if (isset($_POST["afrond"])){
+    $sql = "UPDATE `opdrachten` SET `afgerond`= 1 WHERE ID LIKE ".$_POST["afrond"];
+    $conn->query($sql) or die("Error : " . mysqli_error($conn));
+}
 $sql = "SELECT opdrachten.ID, Bedrijfsnaam,Opdrachtnaam,Opdrachtbeschrijving FROM `opdrachten` WHERE `Bedrijfsnaam` like 'echtbedrijf' and `afgerond` like 0";
 $result = $conn->query($sql);
-echo "<form action='foto%20toevoegen.php' method='post'>";
 echo "<table>";
 echo "<tr><th>bedrijfsnaam</th><th>opdrachtnaam</th><th>opdrachtbeschijving</th></tr>";
 while ($row= $result->fetch_assoc()){
+    echo "<form action='foto%20toevoegen.php' method='post'>";
     echo "<tr>";
     echo  "<td>".$row["Bedrijfsnaam"]."</td>";
     echo  "<td>".$row["Opdrachtnaam"]."</td>";
     echo "<td><div class=\"button_cont\" align=\"center\"><button class=\"example_a\" rel=\"nofollow noopener\" type='button' onclick='fer".$row["ID"]."()'>meerinfo</button></td>";
-    echo "<td>  <div class=\"button_cont\" align=\"center\"><button type='submit' value='".$row["ID"]."'  name='opID2' class=\"example_a\" rel=\"nofollow noopener\">foto toevoegen</button></div>
-</div></td>";
+    echo "<td>  <div class=\"button_cont\" align=\"center\"><button type='submit' value='".$row["ID"]."'  name='opID2' class=\"example_a\" rel=\"nofollow noopener\">foto toevoegen</button></div></div></td></form>";
+    echo "<td><form action='mainpage.php' method='post'> <div class=\"button_cont\" align=\"center\"><button type='submit' value='".$row["ID"]."'  name='afrond' class=\"example_a\" rel=\"nofollow noopener\">afronden</button></div></form></td>  ";
 
     echo "</tr>";
     echo "<script> function fer".$row["ID"]."() {
     alert('".$row["Opdrachtbeschrijving"]."')
 }</script>";
 }
-echo "</table></form>"
+echo "</table>"
 ?>
